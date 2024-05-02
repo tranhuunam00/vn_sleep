0213a6ea98b45831bded6dd77ba09726a9b6abd6
namth1 <namth1@vuihoc.vn>
2024-05-02 11:48:47 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,8 +18,7 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
-import { MailerModule } from '@nestjs-modules/mailer';
-
+import { GmailModule } from './modules/gmail/gmail.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -27,31 +26,9 @@ import { MailerModule } from '@nestjs-modules/mailer';
       load: [configuration],
       // envFilePath: ['.env.development.local', '.env.development'],
     }),
-    MailerModule.forRoot({
-      transport: {
-        // host: 'smtp.example.com',
-        service: 'gmail',
-        secure: false,
-        // port: 587,
-        auth: {
-          type: 'OAuth2',
-          user: process.env.ADMIN_EMAIL_ADDRESS,
-          clientId: process.env.GOOGLE_MAILER_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_MAILER_CLIENT_SECRET,
-          refreshToken: process.env.GOOGLE_MAILER_REFRESH_TOKEN,
-        },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@example.com>',
-      },
-      template: {
-        options: {
-          strict: true,
-        },
-      },
-    }),
     JwtModule.register({ secret: process.env.JWT_SECRET }),
     MongooseModule.forRoot(process.env.MONGO_DB_URL),
+    GmailModule,
     UserModule,
     AuthModule,
   ],
